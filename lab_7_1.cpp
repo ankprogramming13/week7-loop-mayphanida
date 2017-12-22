c9fd1b3c050fabbd9cc33431980f1c81eedb2614
Phanida Thaijamnong <phanidatha@cvc-cha.ac.th>
2017-12-22 13:29:37 +0700

    ทำข้อ 1

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-  
+
   cout << "This is simple loop"<< endl << endl ;
 
   int i = 0;
@@ -14,14 +14,10 @@ int main() {
   }
 
   // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
-  cout << "Hello World 1" << endl;
-  cout << "Hello World 2" << endl;
-  cout << "Hello World 3" << endl;
-  cout << "Hello World 4" << endl;
-  cout << "Hello World 5" << endl;
-  cout << "Hello World 6" << endl;
-  cout << "Hello World 7" << endl;
-  cout << "Hello World 8" << endl;
+  for(i = 0; i < 10 ; i++) {
+    cout << "Hello World" << i << endl;
+
+  }
 
   return 0;
 }
